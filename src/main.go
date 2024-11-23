ce1a2e09e8fcce40a984a308a1ea48281295126e
Subaru Sasaki <subaru.sasaki@i-zuna.co.jp>
2024-11-23 21:31:27 +0900

    refactor: main.go

--- src/main.go
+++ src/main.go
@@ -10,8 +10,8 @@ func HelloWord(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/",HelloWord)
-	fmt.Println("Server is running on port 3000");
+	http.HandleFunc("/", HelloWord)
+	fmt.Println("Server is running on port 3000")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		fmt.Println(err)
