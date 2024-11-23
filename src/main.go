62023a2ebd3cdbfc5d182b9bd90e3318d4a4e274
Subaru Sasaki <subaru.sasaki@i-zuna.co.jp>
2024-11-23 20:51:48 +0900

    add: port3000のserverを起動

--- src/main.go
+++ src/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func HelloWord(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/",HelloWord)
+	fmt.Println("Server is running on port 3000");
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
